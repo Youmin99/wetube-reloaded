1c8185728207328ac3f20066164d567be8e5f023
youmin <105027461+futureyp@users.noreply.github.com>
2023-02-22 22:12:51 -0800

    wetube

---
+++ src/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+import apiRouter from "./routers/apiRouter";
+import flash from "express-flash";
+import { localsMiddleware } from "./middlewares";
+import "dotenv/config";
+import "./db";
+import "./models/Video";
+import "./models/User";
+import "./models/Comment";
+
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+app.use(flash());
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
+app.use("/", rootRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter); 
+app.use("/api", apiRouter);
+
+const PORT = 5000;
+const handleListening = () =>
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
