1d755c262cff39a6fe12e95e671edd67328af0ed
youmin <105027461+futureyp@users.noreply.github.com>
2023-05-10 19:33:55 -0700

    filesize up

--- src/middlewares.js
+++ src/middlewares.js
@@ -28,12 +28,12 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: {
-    fileSize: 3000000,
+    fileSize: 30000000,
   },
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
-    fileSize: 10000000,
+    fileSize: 1000000000000000,
   },
 });
